7e0663ca31bc2e0f62b9bc11e73df8d49698cd5d
Albusaidy Yassin <albusaidy05@gmail.com>
2023-06-05 20:20:58 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         textTheme: TextTheme(
           labelLarge: const TextStyle(
               fontSize: 17,
-              fontFamily: 'Manrope',
               fontWeight: FontWeight.w600,
               height: 1.2,
               color: Color(0xFF211D1D)),
@@ -33,19 +32,16 @@ class MyApp extends StatelessWidget {
               height: 1.2),
           titleMedium: const TextStyle(
               fontSize: 17,
-              fontFamily: 'Manrope',
               fontWeight: FontWeight.w600,
               height: 1.2,
               color: Color(0xFFF8F8F8)),
           titleLarge: const TextStyle(
             fontSize: 32,
-            fontFamily: 'Manrope',
             fontWeight: FontWeight.w500,
             color: Color(0xFFF8F8F8),
           ),
           titleSmall: const TextStyle(
             fontSize: 12,
-            fontFamily: 'Manrope',
             fontWeight: FontWeight.w500,
             color: Color(0x66F8F8F8),
           ),
